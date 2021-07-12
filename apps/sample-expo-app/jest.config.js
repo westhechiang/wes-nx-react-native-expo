083f75056fd7b68cdbade0ef55ad2a0a7d7ef911
Wesley Chiang <wes.chiang@sweetgreen.com>
2021-07-12 14:17:56 -0700

    feat(sample-expo-app): Create expo app

---
+++ apps/sample-expo-app/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  displayName: 'sample-expo-app',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/sample-expo-app',
+};
