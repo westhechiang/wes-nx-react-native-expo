0e3cc763ee8c5e84df517c4b9cddb8e0c4962b3b
Wesley Chiang <wes.chiang@sweetgreen.com>
2021-07-12 17:33:48 -0700

    fix(sample-expo-app): Use defaultConfig only

--- apps/sample-expo-app/metro.config.js
+++ apps/sample-expo-app/metro.config.js
@@ -1,5 +1,4 @@
-const { withNxMetro } = require('nx-react-native-expo');
 const { getDefaultConfig } = require('@expo/metro-config');
 
 const defaultConfig = getDefaultConfig(__dirname);
-module.exports = withNxMetro(defaultConfig);
+module.exports = defaultConfig;
